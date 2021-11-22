e0e6b43cb28803bd379dfaf271a6e5bbe5dcfd01
mohamed-elfiky <elfikymohamedelfiky@gmail.com>
2021-11-22 15:20:19 +0200

    chore: modified devops

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -8,7 +8,7 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('helloo')
+  @Get('hello')
   getData(): Message {
     return this.appService.getData();
   }
